88142e5a015264480a55d046feb560b3846d1392
Sanjay-adepu <adepusanjay812@gmail.com>
2025-03-09 10:46:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"]
 }));
 app.use(express.json());
 
