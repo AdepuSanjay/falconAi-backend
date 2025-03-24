4ff3173767844c0ad423aeaf89b33f81bd936d9e
Sanjay-adepu <adepusanjay812@gmail.com>
2025-03-24 18:37:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ const PPTX2Json = require("pptx2json");
 
  
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "http://localhost:5173" }
+});
+
+
 app.use(cors({ origin: "http://localhost:5173", methods: ["GET", "POST"] }));
 app.use(express.json());
 
